708543cf92602b434e1481bdfb9cec425d4dde9d
ajrun20 <afsanaajrun@gmail.com>
2024-01-13 19:11:58 +0530

    new file

---
+++ intro.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a = 10;
+	int b= 10;//add two numbers in c++
+	cout << "ADDING TWO NUMBERS "<<endl;
+	cout << "answer is :" << a+b ;
+	return 0;
+}
